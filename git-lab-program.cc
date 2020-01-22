f51e42b864d19ffec673397c12ef89d95482a003
Shelby Engle <se352717@ohio.edu>
2020-01-21 19:36:14 -0500

    Commiting :)

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Shelby Engle
+ *        Date: January 21, 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
